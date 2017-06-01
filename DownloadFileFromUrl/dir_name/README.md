6c8c9bf9ade7c27c38ca1cb285ab2179b999b86b
JNingWei <git@github.com>
2017-06-01 11:24:56 +0800

    branch

---
+++ DownloadFileFromUrl/dir_name/README.md
@@ -0,0 +1,1 @@
+dir 'dir_name/' for saving the file you need or you will download.
---
+++ DownloadFileFromUrl/down.py
@@ -0,0 +1,33 @@
+import os
+import urllib
+import logging
+import sys
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(message)s',
+    level=logging.INFO,
+    stream=sys.stdout)
+
+file_path = os.path.join(os.getcwd(),'dir_name/file_name')
+
+if not os.path.isfile(file_path):
+    logging.info("File doesn't exist.")
+    # replace with url you need
+    url = 'https://github.com/JNingWei/Notebook/blob/master/README.md'
+
+    # if dir 'dir_name/' doesn't exist
+    file_dir = file_path[:-9]
+    if not os.path.exists(file_dir):
+        logging.info("Mkdir 'dir_name/'.")
+        os.mkdir(file_dir)
+
+    def down(_save_path, _url):
+        try:
+            urllib.urlretrieve(_url, _save_path)
+        except:
+            print '\nError when retrieving the URL:', _save_path
+
+    logging.info("Downloading file.")
+    down(file_path, url)
+else:
+    logging.info("File exists.")
